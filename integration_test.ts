5c8cd04752f6bb710bbedb00abe60ec97b924f18
uki00a <uki00a@gmail.com>
2020-11-09 22:17:38 +0900

    chore: cleanup integration tests (#6)

--- integration_test.ts
+++ integration_test.ts
@@ -5,236 +5,119 @@ import {
   assertStrictEquals,
   path,
 } from "./deps.ts";
-import { readResponse, sendMessage, withTimeout } from "./test_utils.ts";
+import { readResponse, sendMessage, testLSP } from "./test_utils.ts";
 
-Deno.test(
-  "integration test",
-  withTimeout(10 * 1000, async () => {
-    let seqId = 0;
-    const projectRoot = Deno.cwd();
-    const cli = Deno.run({
-      cmd: [
-        "deno",
-        "run",
-        "--allow-read",
-        "--allow-write",
-        "--quiet",
-        "cli.ts",
-      ],
-      cwd: projectRoot,
-      stdout: "piped",
-      stdin: "piped",
-    });
-    const { stdin, stdout } = cli;
-    assert(stdin);
-    assert(stdout);
-
-    try {
-      await sendMessage(stdin, {
-        "id": ++seqId,
+testLSP({
+  name: "hover",
+  async fn(lsp) {
+    const testTSFile = path.join(lsp.projectRoot, "add.ts");
+    await sendMessage(
+      lsp.stdin,
+      {
+        "method": "textDocument/didOpen",
         "jsonrpc": "2.0",
-        "method": "initialize",
         "params": {
-          "rootUri": path.toFileUrl(projectRoot).href,
-          "capabilities": {
-            "workspace": { "configuration": true, "applyEdit": true },
-            "textDocument": {
-              "implementation": { "linkSupport": true },
-              "documentSymbol": {
-                "symbolKind": {
-                  "valueSet": [
-                    10,
-                    11,
-                    12,
-                    13,
-                    14,
-                    15,
-                    16,
-                    17,
-                    18,
-                    19,
-                    20,
-                    21,
-                    22,
-                    23,
-                    24,
-                    25,
-                    26,
-                    1,
-                    2,
-                    3,
-                    4,
-                    5,
-                    6,
-                    7,
-                    8,
-                    9,
-                  ],
-                },
-                "hierarchicalDocumentSymbolSupport": false,
-              },
-              "semanticHighlightingCapabilities": {
-                "semanticHighlighting": false,
-              },
-              "codeAction": {
-                "codeActionLiteralSupport": {
-                  "codeActionKind": {
-                    "valueSet": [
-                      "",
-                      "quickfix",
-                      "refactor",
-                      "refactor.extract",
-                      "refactor.inline",
-                      "refactor.rewrite",
-                      "source",
-                      "source.organizeImports",
-                    ],
-                  },
-                },
-                "dynamicRegistration": false,
-              },
-              "completion": {
-                "completionItem": {
-                  "snippetSupport": false,
-                  "documentationFormat": ["plaintext"],
-                },
-                "completionItemKind": {
-                  "valueSet": [
-                    10,
-                    11,
-                    12,
-                    13,
-                    14,
-                    15,
-                    16,
-                    17,
-                    18,
-                    19,
-                    20,
-                    21,
-                    22,
-                    23,
-                    24,
-                    25,
-                    1,
-                    2,
-                    3,
-                    4,
-                    5,
-                    6,
-                    7,
-                    8,
-                    9,
-                  ],
-                },
-              },
-              "foldingRange": { "lineFoldingOnly": true },
-              "typeDefinition": { "linkSupport": true },
-              "typeHierarchy": false,
-              "declaration": { "linkSupport": true },
-              "definition": { "linkSupport": true },
-            },
-          },
-          "rootPath": projectRoot,
-          "processId": Deno.pid,
-          "trace": "off",
-        },
-      });
-
-      const initializeResult = await readResponse(stdout);
-      assertObjectMatch(initializeResult, {
-        jsonrpc: "2.0",
-        id: seqId,
-        result: {
-          serverInfo: { name: "deno-lsp" },
-        },
-      });
-
-      const testTSFile = path.join(projectRoot, "/testdata/add.ts");
-      await sendMessage(
-        stdin,
-        {
-          "method": "textDocument/didOpen",
-          "jsonrpc": "2.0",
-          "params": {
-            "textDocument": {
-              "uri": path.toFileUrl(testTSFile).href,
-              "version": 1,
-              "languageId": "typescript",
-              "text": await Deno.readTextFile(
-                testTSFile,
-              ),
-            },
+          "textDocument": {
+            "uri": path.toFileUrl(testTSFile).href,
+            "version": 1,
+            "languageId": "typescript",
+            "text": await Deno.readTextFile(
+              testTSFile,
+            ),
           },
         },
-      );
+      },
+    );
 
-      await sendMessage(
-        stdin,
-        {
-          "id": ++seqId,
-          "jsonrpc": "2.0",
-          "method": "textDocument/hover",
-          "params": {
-            "textDocument": {
-              "uri": path.toFileUrl(testTSFile).href,
-            },
-            "position": { "character": 0, "line": 0 },
-          },
-        },
-      );
-
-      const hover = await readResponse(stdout);
-      assertEquals(hover, {
-        jsonrpc: "2.0",
-        id: seqId,
-        result: {
-          contents: {
-            language: "typescript",
-            value: "function add(a: number, b: number): number",
-          },
-        },
-      });
-
-      await sendMessage(stdin, {
-        "id": ++seqId,
+    const nextID = lsp.nextID();
+    await sendMessage(
+      lsp.stdin,
+      {
+        "id": nextID,
         "jsonrpc": "2.0",
-        "method": "textDocument/definition",
+        "method": "textDocument/hover",
         "params": {
           "textDocument": {
             "uri": path.toFileUrl(testTSFile).href,
           },
-          "position": {
-            "line": 0,
-            "character": 1,
+          "position": { "character": 0, "line": 0 },
+        },
+      },
+    );
+
+    const hover = await readResponse(lsp.stdout);
+    assertEquals(hover, {
+      jsonrpc: "2.0",
+      id: nextID,
+      result: {
+        contents: {
+          language: "typescript",
+          value: "function add(a: number, b: number): number",
+        },
+      },
+    });
+  },
+  projectRoot: path.resolve("testdata/hover"),
+});
+
+testLSP({
+  name: "definition",
+  async fn(lsp) {
+    const testTSFile = path.join(lsp.projectRoot, "sample.ts");
+    await sendMessage(
+      lsp.stdin,
+      {
+        "method": "textDocument/didOpen",
+        "jsonrpc": "2.0",
+        "params": {
+          "textDocument": {
+            "uri": path.toFileUrl(testTSFile).href,
+            "version": 1,
+            "languageId": "typescript",
+            "text": await Deno.readTextFile(
+              testTSFile,
+            ),
           },
         },
-      });
+      },
+    );
 
-      const definitions = await readResponse(stdout);
-      assertEquals(definitions, {
-        id: seqId,
-        jsonrpc: "2.0",
-        result: [
-          {
-            uri: path.toFileUrl(testTSFile).href,
-            range: {
-              start: {
-                line: 2,
-                character: 16,
-              },
-              end: {
-                line: 2,
-                character: 18,
-              },
+    const nextID = lsp.nextID();
+    await sendMessage(lsp.stdin, {
+      "id": nextID,
+      "jsonrpc": "2.0",
+      "method": "textDocument/definition",
+      "params": {
+        "textDocument": {
+          "uri": path.toFileUrl(testTSFile).href,
+        },
+        "position": {
+          "line": 0,
+          "character": 1,
+        },
+      },
+    });
+
+    const definitions = await readResponse(lsp.stdout);
+    assertEquals(definitions, {
+      id: nextID,
+      jsonrpc: "2.0",
+      result: [
+        {
+          uri: path.toFileUrl(testTSFile).href,
+          range: {
+            start: {
+              line: 6,
+              character: 16,
+            },
+            end: {
+              line: 6,
+              character: 18,
             },
           },
-        ],
-      });
-    } finally {
-      cli.stdin.close();
-      cli.stdout.close();
-      cli.close();
-    }
-  }),
-);
+        },
+      ],
+    });
+  },
+  projectRoot: path.resolve("testdata/definition"),
+});
---
+++ testdata/definition/sample.ts
@@ -0,0 +1,9 @@
+add(1, 2);
+
+export function subtract(a: number, b: number): number {
+  return a - b;
+}
+
+export function add(a: number, b: number): number {
+  return a + b;
+}
